a057cd040d27e804409a246306606bf78a67c3b8
MeryemMaftouh <meryemmaftouh744@gmail.com>
2025-11-07 19:13:41 +0100

    solve problems

--- prodev-mobile-app-0x05/app/(home)/index.tsx
+++ prodev-mobile-app-0x05/app/(home)/index.tsx
@@ -1,8 +1,16 @@
-import PropertyListing from "../../components/PropertyListing";
-import { styles } from "../../styles/_homestyle";
+import PropertyListing from "@/components/PropertyListing";
+import { styles } from "@/styles/_homestyle";
 import { Feather } from "@expo/vector-icons";
-import { View, Text, TextInput, Image, ScrollView, TouchableHighlight } from "react-native";
-import { FILTERS, SAMPLE_DATA } from "../../constants/data";
+import {
+  View,
+  Text,
+  TextInput,
+  Image,
+  ScrollView,
+  Dimensions,
+  TouchableHighlight,
+} from "react-native";
+import { FILTERS, SAMPLE_DATA } from "@/constants/data";
 
 const Home = () => {
   return (
@@ -22,13 +30,20 @@ const Home = () => {
         </View>
       </View>
 
-      <View style={{ height: 72, backgroundColor: "white" }}>
+      <View
+        style={{
+          height: 72,
+          backgroundColor: "white",
+        }}
+      >
         <ScrollView horizontal showsHorizontalScrollIndicator={false}>
           <View style={styles.filterGroup}>
             {FILTERS.map((filter, index) => (
               <View style={styles.filterContainer} key={index}>
                 <Image
-                  style={{ flex: 1 }}
+                  style={{
+                    flex: 1,
+                  }}
                   source={require("@/assets/images/mansion.png")}
                   resizeMode="contain"
                 />
