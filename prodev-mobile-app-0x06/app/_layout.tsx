f80d384a2e59221c6068c3fc5a6ac579e9eab135
MeryemMaftouh <meryemmaftouh744@gmail.com>
2025-11-09 18:25:58 +0100

    test

--- prodev-mobile-app-0x06/app/_layout.tsx
+++ prodev-mobile-app-0x06/app/_layout.tsx
@@ -1,16 +1,2 @@
-import { Stack } from "expo-router";
-import { ScrollView } from "react-native";
-import "../styles/global.css";
-
-
-export default function RootLayout() {
-  return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="index" />
-      <Stack.Screen name="join" />
-      <Stack.Screen name="signin" />
-      <Stack.Screen name="(home)" />
-
-    </Stack>
-  );
-}
+// app/_layout.tsx
+export { default } from "./(home)/_layout";
